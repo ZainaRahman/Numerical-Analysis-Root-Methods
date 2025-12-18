a06b47770a735434caba47c320138ea0cef9ff9d
Zaina Rahman <rahman2207021@stud.kuet.ac.bd>
2025-12-19 02:05:51 +0600

    updated the matrix inversion code for corrected output

--- Linear Systems/Matrix Inversion/Matrix_Inversion_Method.cpp
+++ Linear Systems/Matrix Inversion/Matrix_Inversion_Method.cpp
@@ -1,156 +1,131 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+// Cofactor
+void getCofactor(const vector<vector<double>>& A,
+                 vector<vector<double>>& temp,
+                 int p, int q, int n)
+{
+    int i = 1, j = 1;
+    for (int row = 1; row <= n; row++) {
+        for (int col = 1; col <= n; col++) {
+            if (row != p && col != q) {
+                temp[i][j++] = A[row][col];
+                if (j == n) {
+                    j = 1;
+                    i++;
+                }
+            }
+        }
+    }
+}
 
-    // ---- File Handling ----
-    ifstream in("input.txt");
-    ofstream out("output.txt");
+//Recursive Determinant
+double determinant(const vector<vector<double>>& A, int n)
+{
+    if (n == 1)
+        return A[1][1];
 
-    if (!in) {
-        cerr << "Error: input.txt not found\n";
+    double det = 0;
+    int sign = 1;
+    vector<vector<double>> temp(n, vector<double>(n, 0));
+
+    for (int i = 1; i <= n; i++) {
+        getCofactor(A, temp, 1, i, n);
+        det += sign * A[1][i] * determinant(temp, n - 1);
+        sign = -sign;
+    }
+    return det;
+}
+
+int main()
+{
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+
+    if (!fin) {
+        cout << "Error opening input file.\n";
         return 1;
     }
 
     int n;
-    in >> n;
+    fin >> n;
 
-    int k = 1;  // B is always n x 1
-
-    // Augmented matrix (n x (n+1))
     vector<vector<double>> aug(n+1, vector<double>(n+2, 0));
-
-    // A matrix (n x n)
     vector<vector<double>> a(n+1, vector<double>(n+1, 0));
-
-    // B matrix (n x 1)
-    vector<vector<double>> B(n+1, vector<double>(k+1, 0));
-
+    vector<vector<double>> B(n+1, vector<double>(2, 0));
     vector<vector<double>> C(n+1, vector<double>(n+1, 0));
     vector<vector<double>> C1(n+1, vector<double>(n+1, 0));
-    vector<vector<double>> res(n+1, vector<double>(k+1, 0));
+    vector<vector<double>> res(n+1, vector<double>(2, 0));
 
-
-    // ----- Read Augmented Matrix -----
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= n + 1; j++)
-            in >> aug[i][j];
+            fin >> aug[i][j];
 
-    // Split into A and B
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++)
             a[i][j] = aug[i][j];
-
         B[i][1] = aug[i][n+1];
     }
 
-    // ------------------ Determinant ------------------
-    int det1, ans = 0;
-    int i = 1;
+    double detA = determinant(a, n);
 
-    for (int j = 1; j <= n; j++) {
-        vector<int> R1, Cc1;
-
-        for (int r = 1; r <= n; r++)
-            if (r != i) R1.push_back(r);
-
-        for (int c = 1; c <= n; c++)
-            if (c != j) Cc1.push_back(c);
-
-        det1 = a[R1[0]][Cc1[0]] * a[R1[1]][Cc1[1]]
-             - a[R1[0]][Cc1[1]] * a[R1[1]][Cc1[0]];
-
-        if ((i + j) % 2 != 0)
-            det1 = -det1;
-
-        det1 *= a[i][j];
-        ans += det1;
-    }
-
-    // ------------------ DET = 0 → check solution type ------------------
-    if (ans == 0) {
-
-        bool noSolution = false, infinite = true;
+    if (detA == 0) {
+        bool noSol = false, infiniteSol = true;
 
         for (int r = 1; r <= n; r++) {
-            bool allZeroA = true;
-
-            for (int c = 1; c <= n; c++) {
+            bool allZero = true;
+            for (int c = 1; c <= n; c++)
                 if (aug[r][c] != 0)
-                    allZeroA = false;
-            }
+                    allZero = false;
 
-            if (allZeroA && aug[r][n+1] != 0) {
-                noSolution = true;
-                infinite = false;
+            if (allZero && aug[r][n+1] != 0) {
+                noSol = true;
+                infiniteSol = false;
                 break;
             }
-            if (!allZeroA)
-                infinite = false;
+            if (!allZero)
+                infiniteSol = false;
         }
 
-        if (noSolution) {
-            out << "Determinant = 0 → No Solution (Inconsistent System)\n";
-        }
-        else {
-            out << "Determinant = 0 → Infinite Solutions (Dependent System)\n";
-        }
+        if (noSol)
+            fout << "Determinant = 0 → No Solution (Inconsistent System)\n";
+        else
+            fout << "Determinant = 0 → Infinite Solutions (Dependent System)\n";
 
         return 0;
     }
 
-    out << "Inverse exists. Determinant = " << ans << "\n\n";
+    fout << "Determinant = " << detA << "\n\n";
 
-    // ------------------ Cofactor Matrix ------------------
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
-            vector<int> R, Cc;
-
-            for (int r = 1; r <= n; r++)
-                if (r != i) R.push_back(r);
-
-            for (int c = 1; c <= n; c++)
-                if (c != j) Cc.push_back(c);
-
-            int det = a[R[0]][Cc[0]] * a[R[1]][Cc[1]]
-                    - a[R[0]][Cc[1]] * a[R[1]][Cc[0]];
-
-            if ((i + j) % 2 != 0)
-                det = -det;
-
-            C[i][j] = det;
+            vector<vector<double>> temp(n, vector<double>(n, 0));
+            getCofactor(a, temp, i, j, n);
+            C[i][j] = pow(-1, i + j) * determinant(temp, n - 1);
         }
     }
 
-    out << "Cofactor Matrix:\n";
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++)
-            out << C[i][j] << " ";
-        out << "\n";
-    }
-
-    out << "\nAdjoint / Inverse Matrix:\n";
+    fout << "Inverse Matrix:\n";
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
-            C1[i][j] = C[j][i] / ans;
-            out << C1[i][j] << " ";
+            C1[i][j] = C[j][i] / detA;
+            fout << C1[i][j] << " ";
         }
-        out << "\n";
+        fout << "\n";
     }
 
-    // ------------------ Multiply Inverse * B ------------------
     for (int i = 1; i <= n; i++) {
-        double sum = 0;
-
         for (int t = 1; t <= n; t++)
-            sum += C1[i][t] * B[t][1];
-
-        res[i][1] = sum;
+            res[i][1] += C1[i][t] * B[t][1];
     }
 
-    out << "\nSolution Vector X:\n";
+    fout << "\nSolution Vector:\n";
     for (int i = 1; i <= n; i++)
-        out << "x" << i << " = " << res[i][1] << "\n";
+        fout << "x" << i << " = " << res[i][1] << "\n";
+
+    fin.close();
+    fout.close();
 
     return 0;
 }
